a62dca54dc159958c36fb19faa7266ce5937049e
JanTHorstmann <janhorstmann@yahoo.de>
2026-01-22 07:33:57 +0100

    add Font 'Inter' add component Navbar, Introduction, UpcommingDates to Dashboard and style and fill content in Navbar and Introduction

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../shared/components/navbar/navbar';
+import { Introduction } from '../introduction/introduction';
+import { UpcomingDates } from '../upcoming-dates/upcoming-dates';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    Navbar,
+    Introduction,
+    UpcomingDates,
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
