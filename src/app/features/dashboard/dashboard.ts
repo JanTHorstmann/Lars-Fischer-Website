50ce288022c804d6cb9ad001468d34bd10a09a89
JanTHorstmann <janhorstmann@yahoo.de>
2026-02-09 04:19:44 +0100

    change folder of video component and add preview video to dahsboard

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -3,6 +3,7 @@ import { Navbar } from '../../shared/components/navbar/navbar';
 import { Introduction } from '../introduction/introduction';
 import { UpcomingDates } from '../upcoming-dates/upcoming-dates';
 import { Footer } from '../../shared/components/footer/footer';
+import { PreviewVideos } from "../preview-videos/preview-videos";
 
 @Component({
   selector: 'app-dashboard',
@@ -11,7 +12,8 @@ import { Footer } from '../../shared/components/footer/footer';
     Introduction,
     UpcomingDates,
     Footer,
-  ],
+    PreviewVideos,
+],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
