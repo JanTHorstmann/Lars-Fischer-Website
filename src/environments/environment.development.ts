ccf733363eafedcdb0173ec08d9178ae754075a1
JanTHorstmann <janhorstmann@yahoo.de>
2026-02-12 20:11:08 +0100

    setup deployment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyA431x0qW0PMwc3Iqv-dyT2MR3NrayB4sw",
+        authDomain: "musikschule-rockin-amadeus.firebaseapp.com",
+        projectId: "musikschule-rockin-amadeus",
+        storageBucket: "musikschule-rockin-amadeus.firebasestorage.app",
+        messagingSenderId: "529604664262",
+        appId: "1:529604664262:web:6763a84b694eb1bce7b0c7",
+        measurementId: "G-R4164JM9RY"
+    }
+};
