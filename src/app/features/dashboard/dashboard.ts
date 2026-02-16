1586ed7a764b51c68b4f07708f3885e2dabfe615
JanTHorstmann <janhorstmann@yahoo.de>
2026-02-16 04:58:42 +0100

    add app-instrument-selection to dashboard

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -4,6 +4,7 @@ import { Introduction } from '../introduction/introduction';
 import { UpcomingDates } from '../upcoming-dates/upcoming-dates';
 import { Footer } from '../../shared/components/footer/footer';
 import { PreviewVideos } from "../preview-videos/preview-videos";
+import { InstrumentSelection } from "../instrument-selection/instrument-selection";
 
 @Component({
   selector: 'app-dashboard',
@@ -13,6 +14,7 @@ import { PreviewVideos } from "../preview-videos/preview-videos";
     UpcomingDates,
     Footer,
     PreviewVideos,
+    InstrumentSelection
 ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
